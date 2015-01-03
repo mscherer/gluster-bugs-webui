e7a56f25f512152addd5ca45481bf50e20d62f55
Niels de Vos <ndevos@redhat.com>
2015-01-03 10:26:32 +0100

    Update run-resport.sh to function as a link from /etc/cron.daily/

--- run-report.sh
+++ run-report.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 #
-# This script is used by a cron job to run the report on a regular basis and refresh the data in a place
-# where the apache server to pick up
+# This script is used by a cron job to run the report on a regular basis and
+# refresh the data in a place where the apache server to pick up
+#
+# Create a symlink under /etc/cron.daily/ and this script will do the rest.
+#
 
-cd ~ubuntu/bugs
+# exit on failure
+set -e
+
+SELF="$(readlink -f ${0})"
+WORKDIR="$(dirname "${SELF}")"
+
+cd ${WORKDIR}
+
 python nova-bugs.py
-cp bugs-refresh.json /var/www/bugs.json
+mv bugs-refresh.json bugs.json
